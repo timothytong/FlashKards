8af4d23506bcd95e450e16bed20aeca946cfeb45
timothytong <timothy.tong@hotmail.ca>
2015-03-23 16:59:16 -0400

    Homepage basic animations complete

--- FlashKards/ViewController.swift
+++ FlashKards/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet var tableView: UITableView!
-    @IBOutlet weak var addCardsButton: UIBarButtonItem!
-    
+    @IBOutlet private var tableView: UITableView!
+    @IBOutlet private weak var addCardsButton: UIBarButtonItem!
+    private var flashcardCollections: Array<FlashCardCollection>!
     override func viewDidLoad() {
         super.viewDidLoad()
         // NavBar
@@ -34,17 +34,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         var homePageCellNib = UINib(nibName: "FlashCardsOverviewCellTemplate", bundle: nil)
         self.tableView.registerNib(homePageCellNib, forCellReuseIdentifier: "homePageCell")
-        self.tableView.dataSource = self;
-        self.tableView.delegate = self;
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.flashcardCollections = [];
+        
+        // Test collections
+        let f1 = FlashCardCollection(collectionName: "漢字", progress: 40, lastReviewed: "2 days ago", numCards: 49)
+        let f2 = FlashCardCollection(collectionName: "ひらがな", progress: 15, lastReviewed: "4 days ago", numCards: 38)
+        let f3 = FlashCardCollection(collectionName: "Vocabularies", progress: 98, lastReviewed: "5 mins ago", numCards: 9)
+        let f4 = FlashCardCollection(collectionName: "Korean", progress: 70, lastReviewed: "2 days ago", numCards: 144)
+        
+        self.flashcardCollections = [f1, f2, f3, f4]
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell:FlashCardsOverviewCell = self.tableView.dequeueReusableCellWithIdentifier("homePageCell") as FlashCardsOverviewCell
-        cell.populateCellWithCollectionName("Hiragana", progress: 85, lastReviewedDate: "Yesterday", numCardsInCollection: 93)
-        return cell;
+        cell.populateCellWithCollection(self.flashcardCollections[indexPath.row])
+        return cell
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.flashcardCollections.count
     }
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 100
@@ -53,7 +63,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
