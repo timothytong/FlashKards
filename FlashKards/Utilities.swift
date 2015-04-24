da8b00da7f6533364d39bf76d5bddeafb5f52ee3
timothytong <timothy.tong@hotmail.ca>
2015-04-23 22:38:52 -0400

    Extracted calculateOptimalFontSizeWithText method to a separate utilities class

--- FlashKards/Utilities.swift
+++ FlashKards/Utilities.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import UIKit
+
+class Utilities {
+    static func calculateOptimalFontSizeWithText(text: String, inRect rect: CGRect) -> CGFloat{
+        // Set the frame of the label to the targeted rectangle
+        println("calculateOptimalFontSizeWithText - \(text)")
+        var fontSize: CGFloat = 100;
+        let minFontSize: CGFloat = 20;
+        let adjustedText = " " + text + " "
+        // Fit label width wize
+        let constraintSize = CGSizeMake(rect.width, CGFloat.max)
+        do {
+            // Set current font size
+            let font = UIFont(name: "AppleSDGothicNeo-Light", size: fontSize)
+            // Find label size for current font size
+            let textRect = (adjustedText as NSString).boundingRectWithSize(constraintSize, options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: [NSFontAttributeName: font!], context: nil)
+            let labelSize = textRect.size
+            // Done, if created label is within target size
+            if labelSize.height <= rect.height{
+                println("  --label width: \(labelSize.width) height \(labelSize.height), rect height \(rect.height)")
+                break
+            }
+            fontSize--
+        } while (fontSize > minFontSize)
+        println("  --returning fontsize \(fontSize)")
+        return fontSize
+    }
+}
