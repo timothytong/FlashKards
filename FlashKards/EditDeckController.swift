bdad206b6adfe6e85af1bc058c0710fda4b42c9f
timothytong <timothy.tong@hotmail.ca>
2015-05-07 00:52:01 -0400

    Added EditDeckController class skeleton

---
+++ FlashKards/EditDeckController.swift
@@ -0,0 +1,14 @@
+//
+//  EditDeckController.swift
+//  FlashKards
+//
+//  Created by Timothy Tong on 2015-05-06.
+//  Copyright (c) 2015 Timothy Tong. All rights reserved.
+//
+
+import UIKit
+
+class EditDeckController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+}
